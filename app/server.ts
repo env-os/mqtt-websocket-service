4706ba61770ea064d054dc5e84b08826be29c33f
RiccardoFio <riccardofio97@gmail.com>
2019-05-24 01:29:00 +0200

    Cambio di Topic funzionante

--- app/server.ts
+++ app/server.ts
@@ -6,7 +6,7 @@ import WebSocket from 'ws';
 
 const brokerUrl = process.env.BROKER_URL || 'mqtt://localhost';
 var topic = process.env.TOPIC || ' ';
-const port = process.env.PORT || 8999;
+const port = process.env.PORT || 8997;
 const app = express();
 
 const server = http.createServer(app);
@@ -14,23 +14,18 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws: WebSocket) => {
+    var client = mqtt.connect(brokerUrl);
+
     ws.on('message', (topic_r: string) => {
-        const client = mqtt.connect(brokerUrl);
-
-        client.on('connect', () => {
-            if(topic!=' '){
-                client.unsubscribe(topic);
-            }
-            topic = process.env.TOPIC || topic_r;
-            client.subscribe(topic);
-        })
-
-        client.on('message', (topic, message) => {
-            ws.send(message.toString());
-        })
-
+        client.unsubscribe(topic);
+        topic = process.env.TOPIC || topic_r;
+        client.subscribe(topic);
     })
-});
+
+    client.on('message', (topic, message) => {
+        ws.send(message.toString());
+    })
+})
 
 server.listen(port, () => {
     console.log(`Server started on port ${port}`);
